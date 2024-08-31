3a67dee6788a4f8ee98c5a3b1e8e135bf1dae4f9
jxcai1006 <jxca2006@hotmail.com>
2024-08-31 14:59:21 -0700

    added links to change language

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ function loadHandler() {
         .then(response => response.text())
         .then(data => {
             document.body.insertAdjacentHTML('afterbegin', data);
+            document.getElementById("toggleLang").addEventListener("click", toggleLang);
         })
         .catch(error => console.error('Error loading header:', error));
         
@@ -12,6 +13,17 @@ function loadHandler() {
         document.body.insertAdjacentHTML('beforeend', data);
     })
     .catch(error => console.error('Error loading footer:', error));
+}
+
+function toggleLang() {
+    let currentUrl = window.location.href;
+    if(!currentUrl.endsWith(".html")) {
+        currentUrl += "index.html";
+    }
+    if (currentUrl.endsWith("index.html")) {
+        const newUrl = currentUrl.replace("index.html", "index_en.html");
+        window.location.href = newUrl;
+    }
 }
 
 window.onload = loadHandler;
--- js/script_en.js
+++ js/script_en.js
@@ -3,6 +3,7 @@ function loadHandler() {
         .then(response => response.text())
         .then(data => {
             document.body.insertAdjacentHTML('afterbegin', data);
+            document.getElementById("toggleLang").addEventListener("click", toggleLang);
         })
         .catch(error => console.error('Error loading header:', error));
         
@@ -12,6 +13,12 @@ function loadHandler() {
         document.body.insertAdjacentHTML('beforeend', data);
     })
     .catch(error => console.error('Error loading footer:', error));
+}
+
+function toggleLang() {
+    const currentUrl = window.location.href;
+    const newUrl = currentUrl.replace("index_en.html", "index.html");
+    window.location.href = newUrl;
 }
 
 window.onload = loadHandler;
